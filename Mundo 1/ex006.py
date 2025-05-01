3e8cad5b984fde1dc7c227152fdd184158715173
João Victor <163316670+vitor981@users.noreply.github.com>
2025-05-01 12:58:22 -0300

    Add files via upload

---
+++ Mundo 1/ex006.py
@@ -0,0 +1,7 @@
+n1 = int(input('Digite um número: '))
+mul = n1 * 2
+tri = n1 * 3
+raiz = n1 ** (1/2)
+print('='*30)
+print('O dobro de {} é: {} \nO triplo é: {} \nE a raiz quadrada é: {:.2f}'.format(n1, mul, tri, raiz))
+print('='*30)
