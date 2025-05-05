0f6cb9cac5bcd2572151d3a7120063993b13c6b1
João Victor <163316670+vitor981@users.noreply.github.com>
2025-05-04 23:32:23 -0300

    Add files via upload

---
+++ Mundo 1/ex009.py
@@ -0,0 +1,29 @@
+from os import system
+
+system('cls')
+
+numero = int(input('Digite o número para ver sua tabuada: '))
+n1 = numero*1
+n2 = numero*2
+n3 = numero*3
+n4 = numero*4
+n5 = numero*5
+n6 = numero*6
+n7 = numero*7
+n8 = numero*8
+n9 = numero*9
+n10 = numero*10
+
+print('='*30)
+print(f'{numero} X 1 = {n1}'
+      f'\n{numero} X 2 = {n2}'
+      f'\n{numero} X 3 = {n3}'
+      f'\n{numero} X 4 = {n4}'
+      f'\n{numero} X 5 = {n5}'
+      f'\n{numero} X 6 = {n6}'
+      f'\n{numero} X 7 = {n7}'
+      f'\n{numero} X 8 = {n8}'
+      f'\n{numero} X 9 = {n9}'
+      f'\n{numero} X 10 = {n10}'  
+    )
+print('='*30)
